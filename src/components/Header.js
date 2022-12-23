23a97a52a88ab125956f159fe1f23efad43926fa
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-23 18:46:43 -0300

    add-icon-guia-add-component-Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+
+  return (
+    <header>
+      <img src="https://cdn-icons-png.flaticon.com/512/3076/3076721.png" alt="logo"/>
+    </header>
+  );
+}
+  
+export default Header;
