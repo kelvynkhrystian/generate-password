bcad1c97dc9e38c717604683e31d276e82728768
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-24 07:45:43 -0300

    add-component-filtros

---
+++ src/components/Filtro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Filtros extends React.Component {
+  render() {
+    return (
+      <section>
+        
+      </section>
+    );
+  }
+}
+
+export default Filtros;
