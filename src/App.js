fa590bf4fc320420fdaed20f2a6e4a2547fc2cf5
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-26 11:02:25 -0300

    test-comments

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Header/>
+        {/* <Header/> */}
         <main>
           <Generate/>
-          <Filtros />
+          {/* <Filtros /> */}
         </main>
         
       </>
--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -7,6 +7,7 @@ class Generate extends React.Component {
 
   generate = async () => {
     const senha = Math.random().toString(36).slice(-10)
+    // const senha = Math.random().toString(32).substr(2)
     console.log('clicou')
     this.setState({
       value: senha,
@@ -17,6 +18,17 @@ class Generate extends React.Component {
     const {value} = this.state;
     return (
       <section className="box-generate">
+        <header>
+        <img
+          src="https://img.freepik.com/icones-gratis/cadeado_318-889192.jpg?w=2000" 
+          alt="logo"
+        />
+        <h2>Gerador de senha aletatória </h2>
+        <img
+          src="https://img.freepik.com/icones-gratis/cadeado_318-889192.jpg?w=2000" 
+          alt="logo"
+        />
+      </header>
         <input type="text" placeholder="clique, gere e copie sua senha" id="input-senha" value={value}/>
         <button onClick={this.generate}><h2>Gerar Senha</h2></button>
       </section>
