3fd06918b83817e9c0af7c8220fab5311f7f478b
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-26 09:28:15 -0300

    add-func-gerador-de-senha-add-state

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 class Generate extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      value: '',
+    }
+  }
+
+  generate = () => {
+    const senha = Math.random().toString(36).slice(-10)
+    console.log('clicou')
+    this.setState({
+      value: senha,
+    })
+  }
+
   render() {
+    const {value} = this.state;
     return (
       <section className="box-generate">
-        <input type="text" placeholder="clique, gere e copie sua senha" id="input-senha" />
-        <button><h2>Gerar Senha</h2></button>
+        <input type="text" placeholder="clique, gere e copie sua senha" id="input-senha" value={value}/>
+        <button onClick={this.generate}><h2>Gerar Senha</h2></button>
       </section>
     );
   }
