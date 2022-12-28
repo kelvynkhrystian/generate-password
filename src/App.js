1c02b18e5c6c7e92c2e6526b4291abc858b118d5
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-27 22:24:48 -0300

    organizando pastas e removendo erros

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Generate from "./components/Generate";
 import Header from "./components/Header";
-import './App.css';
 
 class App extends React.Component {
   render() {
