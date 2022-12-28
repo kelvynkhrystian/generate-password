7a8b414f35f8e73b6d27e32b264f0795ae90e8dd
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-28 13:17:06 -0300

    add-toast

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Section } from '../styles/styles'
 import passwordGenerator from '../utils/passwordGenerator'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class Generate extends React.Component {
   state = {
@@ -9,13 +11,29 @@ class Generate extends React.Component {
     copy: false,
   };
 
+
   setPassword = async () => {
-    const senha = passwordGenerator()
+    
+    const senha = passwordGenerator();
     
     this.setState({
       value: senha,
       copied: false,
     })
+  }
+
+  toast = () => {
+    return toast.success('Senha Copiada', {
+      position: "bottom-center",
+      autoClose: 500,
+      hideProgressBar: true,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    
   }
 
   render() {
@@ -32,7 +50,7 @@ class Generate extends React.Component {
 
           <CopyToClipboard text={value}
             onCopy={() => this.setState({copied: true})}>
-            <img src="https://cdn-icons-png.flaticon.com/512/54/54702.png" alt="copiar"/>
+            <img src="https://cdn-icons-png.flaticon.com/512/54/54702.png" alt="copiar" onClick={this.toast} />
           </CopyToClipboard>
           
         </label>
@@ -40,6 +58,8 @@ class Generate extends React.Component {
         <button onClick={this.setPassword}>
           Generate Password
         </button>
+
+        <p><ToastContainer/></p>
       </Section>
     );
   }
